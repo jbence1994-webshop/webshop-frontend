670715e82ca221e6639d338b9298cc41f011b5dd
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-08 13:00:26 +0200

    Rename main stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./components/App";
+import App from "@components/App.tsx";
 
-import "./index.css";
+import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
