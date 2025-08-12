f037ec49d11e48ab063c4a4d72e14e8334988aa2
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-08-12 16:05:14 +0200

    Temporarily removing product photo fetching from backend (#39)

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,11 +1,6 @@
-import { useEffect, useState } from "react";
-
 import Card from "@components/common/Card";
 
-import { getProductPhotos } from "@services/productPhotoService";
-
 import type { Product } from "@models/product";
-import type { ProductPhoto } from "@models/productPhoto";
 
 import noImage from "@assets/no-image.png";
 
@@ -16,35 +11,12 @@ interface Props {
 const ProductCard = ({ product }: Props) => {
   const { id, name, price, unit } = product;
 
-  const [images, setImages] = useState<ProductPhoto[]>([]);
-
-  useEffect(() => {
-    const getAllProductPhotos = async () => {
-      try {
-        const data = await getProductPhotos(id);
-        setImages(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getAllProductPhotos();
-  }, [id]);
-
-  let image: string;
-
-  if (images.length !== 0) {
-    image = images[0].url;
-  } else {
-    image = noImage;
-  }
-
   return (
     <>
       <Card
         key={id}
         title={name}
-        image={image}
+        image={noImage}
         imageAlt={name}
         contents={[{ key: 1, value: `$${price} / ${unit}` }]}
       />
