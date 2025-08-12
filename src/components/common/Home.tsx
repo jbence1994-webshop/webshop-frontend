ea2bcd4521d2237e4ef35a173ec7e407a3572c20
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-08-12 16:02:40 +0200

    Redesign Home page

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -1,7 +1,12 @@
 const Home = () => {
   return (
-    <section>
-      <p>Home page</p>
+    <section
+      className="d-flex flex-column justify-content-center align-items-center text-center"
+      style={{ height: "calc(100vh - 56px)" }}
+    >
+      <article className="row">
+        <h1 className="display-1 fw-bold">Home page</h1>
+      </article>
     </section>
   );
 };
